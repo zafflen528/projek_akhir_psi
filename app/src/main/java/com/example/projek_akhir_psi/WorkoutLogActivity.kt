55b3e14220fac112abbd3d64a08c92c653340617
zafflen528 <abyan@zafflen.xyz>
2022-11-29 10:18:59 +0700

    added activity to workout log

---
+++ app/src/main/java/com/example/projek_akhir_psi/WorkoutLogActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projek_akhir_psi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutLogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_log)
+    }
+}
