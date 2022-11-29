b2412ba6eb2b580ecb09483949dcfc007223cbbc
saannns <76205974+miftahulihsan@users.noreply.github.com>
2022-11-29 16:02:12 +0700

    Perbaikan recycler view latihan di homepage dan adapternya

--- app/src/main/java/com/example/projek_akhir_psi/JenisLatihan.kt
+++ app/src/main/java/com/example/projek_akhir_psi/JenisLatihan.kt
@@ -3,4 +3,7 @@ package com.example.projek_akhir_psi
 data class JenisLatihan(
     var image: Int,
     var nama: String,
+//    val gambarDetail : Int,
+//    val tahapan : ArrayList<StepWorkout>,
+//    val jumlahTahapan : Int
 )
