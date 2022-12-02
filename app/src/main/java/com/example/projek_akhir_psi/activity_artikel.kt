beb33ecb6665b020c0f48469961d5548c7b9a9f9
notgood54 <84709584+notgood54@users.noreply.github.com>
2022-12-02 19:26:06 +0700

    backup dikit

---
+++ app/src/main/java/com/example/projek_akhir_psi/activity_artikel.kt
@@ -0,0 +1,12 @@
+package com.example.projek_akhir_psi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_artikel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artikel)
+
+    }
+}
